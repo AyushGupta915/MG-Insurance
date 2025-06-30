f65d5e20bdb0964cc4b56f21cc0d4b164c9556b6
Ayush Gupta <135539142+AyushGupta915@users.noreply.github.com>
2025-06-30 11:53:09 +0530

    Update contactRouter.js

--- backend/routes/contactRouter.js
+++ backend/routes/contactRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendContactMail } = require('../controllers/contactController');
+const { sendContactMail } = require('../controller/contactController');
 
 router.post('/contact', sendContactMail);
 module.exports = router;
